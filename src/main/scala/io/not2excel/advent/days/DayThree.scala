1ad0340b061909953badf536d30e31f05ceb5611
Richmond Steele <not2excel@gmail.com>
2015-12-03 10:47:45 -1000

    switched to immutable sets, no real purpose, just dislike having the extra mutable qualification

--- src/main/scala/io/not2excel/advent/days/DayThree.scala
+++ src/main/scala/io/not2excel/advent/days/DayThree.scala
@@ -2,7 +2,7 @@ package io.not2excel.advent.days
 
 import io.not2excel.advent.Main
 
-import scala.collection.mutable
+import scala.collection.immutable.HashSet
 
 object DayThree {
 
@@ -19,8 +19,8 @@ object DayThree {
 
     def partOne() = {
         var x, y = 0
-        val coords: mutable.HashSet[(Int, Int)] = mutable.HashSet()
-        coords.add((x, y))
+        var coords: HashSet[(Int, Int)] = HashSet()
+        coords += ((x, y))
         input.foreach(c => {
             c match {
                 case '<' => x -= 1
@@ -28,7 +28,7 @@ object DayThree {
                 case '>' => x += 1
                 case '^' => y += 1
             }
-            coords.add((x, y))
+            coords += ((x, y))
         })
         println(s"Houses: ${coords.size }")
     }
@@ -36,10 +36,10 @@ object DayThree {
     def partTwo() = {
         var real = true
         var x1, y1, x2, y2 = 0
-        val coords1: mutable.HashSet[(Int, Int)] = mutable.HashSet()
-        val coords2: mutable.HashSet[(Int, Int)] = mutable.HashSet()
-        coords1.add((x1, y1))
-        coords2.add((x2, y2))
+        var coords1: HashSet[(Int, Int)] = HashSet()
+        var coords2: HashSet[(Int, Int)] = HashSet()
+        coords1 += ((x1, y1))
+        coords2 += ((x2, y2))
         input.foreach(c => {
             if(real) {
                 c match {
@@ -48,7 +48,7 @@ object DayThree {
                     case '>' => x1 += 1
                     case '^' => y1 += 1
                 }
-                coords1.add((x1, y1))
+                coords1 += ((x1, y1))
             } else {
                 c match {
                     case '<' => x2 -= 1
@@ -56,7 +56,7 @@ object DayThree {
                     case '>' => x2 += 1
                     case '^' => y2 += 1
                 }
-                coords2.add((x2, y2))
+                coords2 += ((x2, y2))
             }
             real = !real
         })
