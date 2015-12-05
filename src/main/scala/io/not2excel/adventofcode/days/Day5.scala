774f82619fe11cc7ef6b86a71e3345aa7086849b
Richmond Steele <not2excel@gmail.com>
2015-12-04 21:30:10 -1000

    day5 done, rip in wasted time cuz i cant math correct with a typo

---
+++ src/main/scala/io/not2excel/adventofcode/days/Day5.scala
@@ -0,0 +1,47 @@
+package io.not2excel.adventofcode.days
+
+import io.not2excel.adventofcode.Main
+
+
+object Day5 {
+
+    val input = Main.sourceFile("day5.data").getLines().toList
+
+    val vowels = Set('a', 'e', 'i', 'o', 'u')
+    val invalidPhrases = Set("ab", "cd", "pq", "xy")
+
+    def main(args: Array[String]): Unit = {
+        println("Part 1")
+        partOne()
+        println("======")
+        println("Part 2")
+        partTwo()
+        println("======")
+    }
+
+    def partOne() = {
+        var count = 0
+        input.foreach(line => {
+            val tripleVowel = line.count(vowels.contains) >= 3
+            val doubleChar = (0 until line.length).exists(i => line.slice(i, i + 1) == line.slice(i + 1, i + 2))
+            val noInvalids = !invalidPhrases.exists(line.contains)
+            if(tripleVowel && doubleChar && noInvalids) {
+                count += 1
+            }
+        })
+        println(s"Nice String Count: $count")
+    }
+
+    def partTwo() = {
+        var count = 0
+        input.foreach(line => {
+            val sandwich = (0 until line.length - 2).exists(i => line(i) == line(i + 2))
+            val twoDoubleChars = (0 until line.length - 1).exists(i => (line.drop(i + 1) lastIndexOf line.slice(i, i + 2)) > 0)
+            if(sandwich && twoDoubleChars) {
+                count += 1
+            }
+        })
+        println(s"Nice String Count: $count")
+    }
+
+}
