a70a3e39415d8e3fd86afac9b0c9887a5016c5ae
Richmond Steele <not2excel@gmail.com>
2015-12-16 19:47:57 -1000

    day 10 finished

--- src/main/scala/io/not2excel/adventofcode/days/Day10.scala
+++ src/main/scala/io/not2excel/adventofcode/days/Day10.scala
@@ -1,10 +1,11 @@
 package io.not2excel.adventofcode.days
 
-import io.not2excel.adventofcode.Main
-
 object Day10 {
 
-    val data = Main.resource("day10.data").getLines().toList
+    //    val data = Main.resource("day10.data").getLines().toList
+    val data = "1113122113"
+    //It's in day10.data as well
+    var result = data
 
     def main(args: Array[String]) = {
         println("Part 1")
@@ -16,10 +17,41 @@ object Day10 {
     }
 
     def partOne() = {
-
+        var result = this.result
+        (0 until 40).foreach(i => {
+            print(s"$i ")
+            result = countDigits(result)
+        })
+        println(s"\nResult: ${result.length}\n")
+        this.result = result
     }
 
     def partTwo() = {
+        var result = this.result
+        (0 until 10).foreach(i => {
+            print(s"${40 + i} ")
+            result = countDigits(result)
+        })
+        println(s"\nResult: ${result.length}\n")
+    }
 
+    def countDigits(data: String) = {
+        var count, i = 0
+        var n = '0'
+        var result = List[String]()
+        while(i < data.length) {
+            val c = data(i)
+            if(n != c) {
+                if(count > 0) {
+                    result = n.toString :: count.toString :: result
+                }
+                n = c
+                count = 0
+            }
+            count += 1
+            i += 1
+        }
+        result = n.toString :: count.toString :: result
+        result.reverse.mkString
     }
 }
