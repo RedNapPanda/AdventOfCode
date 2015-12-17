23b88be9e2cf2fdc593fcb4859024abd81bcc310
Richmond Steele <not2excel@gmail.com>
2015-12-16 16:20:26 -1000

    added day8 and day9 puzzles, stripped answers from other puzzles

---
+++ src/main/scala/io/not2excel/adventofcode/days/Day10.scala
@@ -0,0 +1,25 @@
+package io.not2excel.adventofcode.days
+
+import io.not2excel.adventofcode.Main
+
+object Day10 {
+
+    val data = Main.resource("day10.data").getLines().toList
+
+    def main(args: Array[String]) = {
+        println("Part 1")
+        partOne()
+        println("======")
+        println("Part 2")
+        partTwo()
+        println("======")
+    }
+
+    def partOne() = {
+
+    }
+
+    def partTwo() = {
+
+    }
+}
